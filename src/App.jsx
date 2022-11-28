bd6d303095077d38c2729f802dcf2150fa98307f
Daniel <dan.red.arteria@gmail.com>
2022-11-27 23:10:24 -0600

    font configurations

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,20 @@ function App() {
 
   return (
     <div className="App">
-     <h1>Fake Forty</h1>
+     <h1 style={{ fontWeight: '100', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '200', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '300', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '400', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '500', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '600', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '700', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '800', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '900', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '300', fontStyle: 'italic', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '400', fontStyle: 'italic', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '500', fontStyle: 'italic', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     <h1 style={{ fontWeight: '700', fontStyle: 'italic', letterSpacing: '-0.27px' }}>Fake Forty F40 GOTHAM EXPLORE</h1>
+     
     </div>
   )
 }
